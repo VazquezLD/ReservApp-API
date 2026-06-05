6d32e4a8e32f1221118dd5fd06d1d1da72c0e39b
Luciano <lucianovazquez.lv15@gmail.com>
2026-06-05 18:46:03 -0300

    fix: adjust express import for serverless compatibility

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 let cachedServer: any;
 
